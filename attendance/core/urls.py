ec222aacfd80db97da19a80ffb58befbfb7f4ccd
Jakub Škápík <112160706+skapis@users.noreply.github.com>
2023-02-27 20:25:38 +0100

    add url to export attendance

--- attendance/core/urls.py
+++ attendance/core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.attendance, name='attendance'),
     path('attendance/<uuid:id>/edit', views.edit_attendance, name='edit_attendance'),
     path('attendance/<uuid:id>/delete', views.delete_attendance, name='delete_attendance'),
-    path('confirm', views.confirm_attendance, name='confirm_attendance')
+    path('confirm', views.confirm_attendance, name='confirm_attendance'),
+    path('export', views.export_attendance, name='export_att')
 ]
