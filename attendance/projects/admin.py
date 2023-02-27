bbc391cd257895b141c207a96854a3aee42801ad
Jakub Škápík <112160706+skapis@users.noreply.github.com>
2023-02-27 21:25:56 +0100

    Update admin.py

--- attendance/projects/admin.py
+++ attendance/projects/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, ProjectsAggMonth
 
 admin.site.register(Project)
-
-
+admin.site.register(ProjectsAggMonth)
